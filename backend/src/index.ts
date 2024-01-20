c81b1c5c470c6045bdd99cf3fba08abb4cfaefc5
otaviootavio <otaviovacari@usp.br>
2024-01-20 01:58:19 -0300

    add login api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,6 @@
-import express from 'express'
+import app from './app'
 
-const app = express()
 const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
